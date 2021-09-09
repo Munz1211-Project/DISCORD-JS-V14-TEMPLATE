982e3f60ba850c51251c9af7571279260d21ce27
Munz1211 <64169740+Munz1211@users.noreply.github.com>
2021-09-10 05:55:44 +0700

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,25 @@
+const chalk = require("chalk");
+const config = require("../config.json");
+
+module.exports = async(client) => {
+  
+        client.user.setPresence({
+                status: "idle"
+        });
+
+        function randomstatus() {
+                let status = [
+                        `WATCHING | Members 👥`,
+                        `PLAYING | Server 🌐`,
+                        `LISTENING | ANYTHING`,
+                        `STREAMING | 24/7 ONLINE...!`
+                ];
+                let rstatus = Math.floor(Math.random() * status.length);
+                client.user.setActivity(status[rstatus], {
+                        type: "PLAYING"
+                });
+        };
+        setInterval(randomstatus, 15000);
+  
+        console.log(chalk.bgBlack.greenBright(`✅ Successfully logged on as ${client.user.username}`));
+}
