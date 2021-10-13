02cdc799937b40a959ed5616ffb592ed480a0606
Munz1211 <64169740+Munz1211@users.noreply.github.com>
2021-10-13 10:18:12 +0700

    Update events.js

--- handlers/events.js
+++ handlers/events.js
@@ -6,29 +6,29 @@ let table = new ascii("Events");
 table.setHeading("Events", "Load status");
 
 module.exports = (client) => {
-  
-        let eventFolder = fs.readdirSync("./events");
-        const eventFiles = fs
-                .readdirSync(`./events/`)
-                .filter((file) => file.endsWith(".js"));
-        for (let file of eventFiles) {
-                try {
-                        const Event = require(`../events/${file}`);
-                        const eventNames = file.split(".")[0];
-                        if (Event.event && typeof Event.event !== "string") {
-                                table.addRow(file, `❌ -> Property event should be string.`);
-                                continue;
-                        }
-                        Event.event = Event.event || file.replace(".js", "")
-                        client.on(file.split(".")[0], (...args) => Event(client, ...args));
-                        table.addRow(file, '✅');
-                } catch (err) {
-                        console.log("Error While loading")
-                        console.log(err)
-                        table.addRow(file, `❌ -> Error while loading event`);
-                }
+
+    let eventFolder = fs.readdirSync("./events");
+    const eventFiles = fs
+        .readdirSync(`./events/`)
+        .filter((file) => file.endsWith(".js"));
+    for (let file of eventFiles) {
+        try {
+            const Event = require(`../events/${file}`);
+            const eventNames = file.split(".")[0];
+            if (Event.event && typeof Event.event !== "string") {
+                table.addRow(file, `❌ -> Property event should be string.`);
+                continue;
+            }
+            Event.event = Event.event || file.replace(".js", "")
+            client.on(file.split(".")[0], (...args) => Event(client, ...args));
+            table.addRow(file, '✅');
+        } catch (err) {
+            console.log("Error While loading")
+            console.log(err)
+            table.addRow(file, `❌ -> Error while loading event`);
         }
-  
-        console.log(table.toString());
-        console.log(chalk.green(`Loaded Successfully [EVENT]`));
+    }
+
+    console.log(table.toString());
+    console.log(chalk.green(`Loaded Successfully [EVENT]`));
 }
