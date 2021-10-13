f7f4477b4cf2a415c56a58eaca6ed0b175c53243
Munz1211 <64169740+Munz1211@users.noreply.github.com>
2021-10-13 10:15:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const discord = require("discord.js");
+require('./server.js');
+const config = require("./config.json");
 
 const client = new discord.Client({
-        restTimeOffset: 0,
-        restWsBridgetimeout: 100,
-        intents: 32767,
-        allowedMentions: {
-                parse: ["users"],
-                repliedUser: true
-        },
-        cacheWithLimits: {
-                MessageManager: {
-                        sweepInterval: 300,
-                        sweepFilter: discord.LimitedCollection.filterByLifetime({
-                                lifetime: 60,
-                                getComparisonTimestamp: m => m.editedTimestamp ?? m.createdTimestamp,
-                        })
-                }
+    restTimeOffset: 0,
+    restWsBridgetimeout: 100,
+    intents: 32767,
+    allowedMentions: {
+        parse: ["users"],
+        repliedUser: true
+    },
+    cacheWithLimits: {
+        MessageManager: {
+            sweepInterval: 300,
+            sweepFilter: discord.LimitedCollection.filterByLifetime({
+                lifetime: 60,
+                getComparisonTimestamp: m => m.editedTimestamp ? ? m.createdTimestamp,
+            })
         }
+    }
 });
 
-require('./server.js');
-
-const config = require("./config.json");
-                          
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
 client.cooldowns = new discord.Collection();
 client.userVoice = new discord.Collection();
-                          
+
 ["commands", "events"].forEach(handler => {
-        require(`./handlers/${handler}`)(client);
+    require(`./handlers/${handler}`)(client);
 });
 
 client.login(config.token).catch(() => { console.log('Invaid TOKEN!') });
