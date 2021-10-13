522cdc90a6d885d13eb9c79193636e13f4e4c122
Munz1211 <64169740+Munz1211@users.noreply.github.com>
2021-10-13 10:19:03 +0700

    Update commands.js

--- handlers/commands.js
+++ handlers/commands.js
@@ -6,25 +6,25 @@ let table = new ascii("Commands");
 table.setHeading("Command", "Load status");
 
 module.exports = (client) => {
-  
-        let folders = fs.readdirSync("./commands/");
-        folders.forEach((dir) => {
-                const commandFiles = fs
-                        .readdirSync(`./commands/${dir}/`)
-                        .filter((file) => file.endsWith(".js"));
-                for (const file of commandFiles) {
-                        const command = require(`../commands/${dir}/${file}`);
-                        if (command.name) {
-                                client.commands.set(command.name, command);
-                                table.addRow(file, '✅');
-                        } else {
-                                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
-                                continue;
-                        }
-                        client.commands.set(command.name, command);
-                }
-        });
-  
-        console.log(table.toString());
-        console.log(chalk.green(`Loaded Successfully [COMMAND]`));
+
+    let folders = fs.readdirSync("./commands/");
+    folders.forEach((dir) => {
+        const commandFiles = fs
+            .readdirSync(`./commands/${dir}/`)
+            .filter((file) => file.endsWith(".js"));
+        for (const file of commandFiles) {
+            const command = require(`../commands/${dir}/${file}`);
+            if (command.name) {
+                client.commands.set(command.name, command);
+                table.addRow(file, '✅');
+            } else {
+                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
+                continue;
+            }
+            client.commands.set(command.name, command);
+        }
+    });
+
+    console.log(table.toString());
+    console.log(chalk.green(`Loaded Successfully [COMMAND]`));
 }
