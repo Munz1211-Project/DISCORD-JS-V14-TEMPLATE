de3f910484445c935cb22a639d5bab662b1810ce
Munz1211 <64169740+Munz1211@users.noreply.github.com>
2021-10-29 17:52:05 +0700

    chore(eval): Change EvalCommand

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,11 +1,13 @@
-const discord = require("discord.js");
-const config = require('../../config.json')
+const discord = require('discord.js');
+const { codeBlock } = require("@discordjs/builders");
+const config = require('../../config.json');
+const petitio = require("petitio");
+const util = require("util");
 
 module.exports = {
 
     name: "eval",
-    category: "owner",
-    aliases: ["e"],
+    aliases: ["ev"],
     description: "evaluation you code.",
     cooldown: 3,
     run: async (client, message, args) => {
@@ -18,51 +20,46 @@ module.exports = {
 
         const msg = message;
         const bot = client;
-        const evalEmbed = new discord.MessageEmbed().setThumbnail(message.author.displayAvatarURL())
-        let code = args.join(" ");
 
         try {
-            const input = clean(code);
+            const code = args.join(" ");
             if (!code) return msg.channel.send("What you'r **JavaScript Codes** ?");
-            evalEmbed.addField("Input", `\`\`\`js\n${input}\n\`\`\``);
-            evalEmbed.setColor(config.color);
-            let evaled;
-            if (code.includes("--silent") && code.includes("--async")) {
-                code = code.replace("--async", "").replace("--silent", "");
-                return await eval(`(async () => { ${code} })()`);
-            } else if (code.includes("--async")) {
-                code = code.replace("--async", "");
-                evaled = await eval(`(async () => { ${code} })()`);
-            } else if (code.includes("--silent")) {
-                code = code.replace("--silent", "");
-                return await eval(code);
-            } else evaled = await eval(code);
-            if (typeof evaled !== "string") evaled = require("util").inspect(evaled, {
-                depth: 0
-            });
+
+            let evaled = eval(code);
+
+            if (typeof evaled !== "string") evaled = util.inspect(evaled, { depth: 0 });
+            evaled = evaled.replace(new RegExp(client.token, "g"), "[Censored]");
+            evaled = evaled.replace(new RegExp(bot.token, "g"), "[Censored]");
+
             let output = clean(evaled);
-            output = output.replace(new RegExp(client.token, "g"), "[TOKEN]");
-            output = output.replace(new RegExp(bot.token, "g"), "[TOKEN]");
-            if (code.includes("--no-embed")) {
-                code = code.replace("--no-embed", "");
-                return msg.channel.send(`\`\`\`js\n${output}\n\`\`\``);
-            }
-            evalEmbed.setColor(config.color);
-            evalEmbed.addField("Output", `\`\`\`js\n${output}\n\`\`\``);
-        } catch (e) {
-            const error = clean(e);
-            if (code.includes("--no-embed")) {
-                code = code.replace("--no-embed", "");
-                return msg.channel.send(`\`\`\`js\n${error}\n\`\`\``);
-            }
-            evalEmbed.setColor("RED");
-            evalEmbed.addField("Error", `\`\`\`js\n${error}\n\`\`\``);
+
+            if (output.length > 1024) {
+                const { key } = await petitio("https://bin.hzmi.xyz/documents", "POST").body(output).json();
+                await msg.channel.send({ 
+                content: "Eval...!",
+                components: [{
+                    "type": 1,
+                    "components": [{
+                        "type": 2,
+                        "label": "Result",
+                        "url": `https://bin.hzmi.xyz/${key}.js`,
+                        "style": 5
+                    }]
+                }]
+            })
+        } else {
+            await msg.channel.send({
+                content: codeBlock("js", evaled)
+            });
         }
-        msg.channel.send({
-            embeds: [evalEmbed]
-        });
+          } catch (e) {
+            const embed = new discord.MessageEmbed()
+                .setDescription(`${e}`)
+                .setColor(config.color)
+            message.channel.send({ embeds: [embed] })
+        }
     }
-};
+}
 
 function clean(text) {
     if (typeof text === "string") return text.replace(/`/g, `\`${String.fromCharCode(8203)}`).replace(/@/g, `@${String.fromCharCode(8203)}`);
